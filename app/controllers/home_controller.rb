d48a7ee45020cc1649905a7c2488d8e94edbe25d
Anna Proios <aproios@colgate.edu>
2018-03-23 03:08:57 +0000

    Updates

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ['BTC', 'XRP',]
+
+
+
+
+  end
+  
+  def about
+  end
+  
+  def lookup
+  end
+  
+  
+end
